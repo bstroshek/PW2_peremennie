5f3c3bb83e829e115eedd0e8c7e768c61015f646
bstroshek <bstroshek@gmail.com>
2021-12-27 21:49:23 +0200

    create index2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let x = 5;
-let userName = 'irina';
-let cat = {
-  name: 'Bars',
-  age: 2
-};
+// let x = 5;
+// let userName = 'irina';
+// let cat = {
+//   name: 'Bars',
+//   age: 2
+// };
 
 // alert(typeof cat);
 
@@ -15,5 +15,18 @@ let cat = {
 // document.write( userName ); 
 // document.write( '<h2>' +userName+ '</h2>' );
 
-document.write( `<h2>${userName}</h2>` );
+//  document.write( `<h2>${userName}</h2>` ); //работает, но может сломать страницу, лучше делать так как показано ниже:
 
+// document //это точка входа в документ
+// document.querySelector('.container').innerHtml = `<h2>${userName}</h2>`;
+// но это можно записать так:
+//доступ к элементу страницы:
+// let container = document.querySelector('.container');
+// console.log(container);
+// container.innerHTML = `<h2>${userName}</h2>`;
+
+
+
+// confirm('Продолжить?');
+
+alert( confirm('Продолжить?') );
